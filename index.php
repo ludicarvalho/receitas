6d2f73ee0173d3dfd5ad8c2b6b42ac7df8d82a0b
Ludi Carvalho <40408321+ludicarvalho@users.noreply.github.com>
2018-11-29 15:25:55 -0200

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $alt = "16"; // Altura da imagem
 <html lang="pt-br">
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=0.9" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="shortcut icon" href="img/favicon.png" />
     <link rel="stylesheet" href="../css/bootstrap.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
@@ -18,9 +18,12 @@ $alt = "16"; // Altura da imagem
     		width: <?php echo $tam; ?>rem;
     		height: <?php echo $alt; ?>rem;
     	}
-      .card {
-        margin: 8px 0px 0px 0px;
-      }
+		.card {
+		margin: 8px 0px 0px 0px;
+		}
+		.footer, h2 {
+			background-color: #F5F5F5;
+		}
     </style>
     <title>Receitas</title>
 </head>
@@ -28,36 +31,37 @@ $alt = "16"; // Altura da imagem
 
 <!-- Menu -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <span class="navbar-brand">Receitas</span>
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="navbar-brand" href="index.php">Início <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="navbar-brand dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Menu
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="criar.php" title="Adicionar nova receita">Cadastrar</a>
-          <div class="dropdown-divider"></div><!-- Divisor de links no menu -->
-		      <a class="dropdown-item" href="../../index.php" title="Sistema Casa Pessoal">Sistema</a>
-          <a class="dropdown-item" href="../agenda/index.php" title="Contatos">Agenda</a>
-          <a class="dropdown-item" href="../">Testes</a>
-        </div>
-      </li>
-    </ul>
-
+	<div class="container">
+		<ul class="navbar-nav">
+			<li>
+				<a class="navbar-brand" href="index.php">Início <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item dropdown">
+				<a class="navbar-brand dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Menu
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item" href="criar.php" title="Adicionar nova receita">Cadastrar</a>
+					<div class="dropdown-divider"></div><!-- Divisor de links no menu -->
+					<a class="dropdown-item" href="../../index.php" title="Sistema Casa Pessoal">Sistema</a>
+					<a class="dropdown-item" href="../agenda/index.php" title="Contatos">Agenda</a>
+					<a class="dropdown-item" href="../">Testes</a>
+				</div>
+			</li>
+		</ul>
+	</div>
 </nav>
 
 <main role="main">
 <!--  Aqui começa a página -->
-<div class="jumbotron">
-  <div class="container">
-    <h1>Receitas Caseiras</h1>
-  </div>
+<div class="jumbotron jumbotron-fluid">
+	<div class="container">
+		<h1>Receitas Juliane</h1>
+	</div>
 </div>
+
 <div class="container">
-<div class="row">
+	<div class="row">
 <?php
 foreach($pdo->query($sql) as $row) {
 if (strlen($row['titulo']) >= 20)
@@ -65,19 +69,20 @@ if (strlen($row['titulo']) >= 20)
 else
 	$string = $row['titulo'];
 ?>
-    <div class="col-md-4">
-        <div class="card" style="width: <?php echo $tam; ?>rem;">
-            <img class="card-img-top" src="<?php echo $row['imagem']; ?>" alt="Imagem de <?php echo ucwords($row['titulo']); ?>" title="<?php echo ucwords($row['titulo']); ?>" />
-                <div class="card-body">
-                    <h5 class="card-title"><b><?php echo ucwords($string); ?></b></h5>
-                    <a href="ler.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">Ver</a>
-                </div>
-        </div>
-    </div>
 
+		<div class="col-md-4"> <!-- Receita <?php echo $row['id']; ?> -->
+			<div class="card" style="width: <?php echo $tam; ?>rem;">
+				<img class="card-img-top" src="<?php echo $row['imagem']; ?>" alt="Imagem de <?php echo ucwords($row['titulo']); ?>" title="<?php echo ucwords($row['titulo']); ?>" />
+				<div class="card-body">
+					<h5 class="card-title"><b><?php echo ucwords($string); ?></b></h5>
+					<a href="ler.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">Ver</a>
+				</div>
+			</div>
+		</div>
 <?php }
 ?>
-</div>
+
+	</div>
 </div>
 
 <br/> <br/>
