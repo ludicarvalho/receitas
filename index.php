14429875c773f0eee92150d4fa37ee039dc64486
Ludi Carvalho <40408321+ludicarvalho@users.noreply.github.com>
2018-12-08 02:26:21 -0200

    Update index.php

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ $alt = "16"; // Altura da imagem
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="shortcut icon" href="img/favicon.png" />
-    <link rel="stylesheet" href="../css/bootstrap.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+    <link rel="stylesheet" href="bootstrap/bootstrap.css" />
+    <script src="bootstrap/jquery.js"></script>
     <style>
     	img {
     		width: <?php echo $tam; ?>rem;
@@ -80,6 +80,7 @@ else
 			</div>
 		</div>
 <?php }
+Banco::desconectar();
 ?>
 
 	</div>
@@ -92,13 +93,13 @@ rodape();
 ?>
 
 </main>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="bootstrap/jquery-3.3.1.slim.min.js"></script>
+<script>window.jQuery || document.write('<script src="bootstrap/jquery-slim.min.js"><\/script>')</script>
+<script src="bootstrap/popper.min.js"></script>
+<script src="bootstrap/bootstrap.min.js"></script>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="https://getbootstrap.com/docs/4.1/assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="https://getbootstrap.com/docs/4.1/assets/js/vendor/popper.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
 </body>
 </html>
