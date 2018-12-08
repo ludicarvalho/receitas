63f256ee801d475bca07d75439132c8cb8ec12c9
Ludi Carvalho <40408321+ludicarvalho@users.noreply.github.com>
2018-12-08 02:02:07 -0200

    Update atualizar.php
    Agora você pode apagar a imagem, atualizar (F5) e colocar outra.

--- atualizar.php
+++ atualizar.php
@@ -1,64 +1,81 @@
 <?php
-	require "banco.php";
+require "banco.php";
 
-	$id = null;
-	if ( !empty($_GET['id'])) {
-		$id = $_REQUEST['id'];
+$id = null;
+if ( !empty($_GET['id'])) {
+	$id = $_REQUEST['id'];
+}
+
+if ( null==$id ) {
+	header("Location: index.php");
+}
+
+if (!empty($_POST)) {
+	$tituloErro = null;
+	$ingredientesErro = null;
+	$modopreparoErro = null;
+
+	$titulo = $_POST['titulo'];
+	$ingredientes = trim($_POST['igrd']);
+	$modopreparo = trim($_POST['modopreparo']);
+
+	//Validação
+	$validacao = true;
+	if (empty($titulo)) {
+		$tituloErro = 'Por favor, digite o nome da Receita!';
+		$validacao = false;
 	}
-	
-	if ( null==$id ) {
-		header("Location: index.php");
+
+	if (empty($ingredientes)) {
+		$ingredientesErro = 'Por favor, digite os ingredientes!';
+		$validacao = false;
 	}
 
-	if ( !empty($_POST)) {
-        $tituloErro = null;
-        $ingredientesErro = null;
-        $modopreparoErro = null;
+	if (empty($modopreparo)) {
+		$modopreparoErro = 'Por favor, digite o modo de preparo!';
+		$validacao = false;
+	}
 
-        $titulo = $_POST['titulo'];
-        $ingredientes = trim($_POST['igrd']);
-        $modopreparo = trim($_POST['modopreparo']);
+	// update data
+	if ($validacao) {
+		$pdo = Banco::conectar();
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$arq = substr($_FILES['imgm']['name'], -4);
+		$inicionome = date('Ymd-His');
+		$pastafoto = "img/$inicionome";
+		$nomefoto = $pastafoto.$arq;
 
-		//Validação
-		$validacao = true;
-    if (empty($titulo)) {
-      $tituloErro = 'Por favor, digite o nome da Receita!';
-      $validacao = false;
-    }
-  
-    if (empty($ingredientes)) {
-      $ingredientesErro = 'Por favor, digite os ingredientes!';
-      $validacao = false;
-    }
+		if(!empty($_FILES['imgm']['name'])) {
+			move_uploaded_file($_FILES['imgm']['tmp_name'], $nomefoto);
 
-    if (empty($modopreparo)) {
-      $modopreparoErro = 'Por favor, digite o modo de preparo!';
-      $validacao = false;
-    }
+			$sql = "UPDATE receitas SET titulo = ?, ingredientes = ?, modopreparo = ?, imagem = ? WHERE id = ?";
+			$q = $pdo->prepare($sql);
+			$q->execute(array($titulo,$ingredientes,$modopreparo,$nomefoto, $id));
+			Banco::desconectar();
 
-		// update data
-		if ($validacao) {
-			$pdo = Banco::conectar();
-			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$sql = "UPDATE `receitas` SET `titulo` = ?, `ingredientes` = ?, `modopreparo` = ? WHERE `id` = ?";
+			header("Location: ler.php?id=$id");
+		}
+		else {
+			$sql = "UPDATE receitas SET titulo = ?, ingredientes = ?, modopreparo = ? WHERE id = ?";
 			$q = $pdo->prepare($sql);
 			$q->execute(array($titulo,$ingredientes,$modopreparo, $id));
 			Banco::desconectar();
 			header("Location: ler.php?id=$id");
 		}
-	} 
-		else {
-			$pdo = Banco::conectar();
-			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$sql = "SELECT * FROM receitas WHERE id = ?";
-			$q = $pdo->prepare($sql);
-			$q->execute(array($id));
-			$data = $q->fetch(PDO::FETCH_ASSOC);
-			$titulo = $data['titulo'];
-			$ingredientes = $data['ingredientes'];
-			$modopreparo = $data['modopreparo'];
-			Banco::desconectar();
-    }
+	}
+}
+else {
+	$pdo = Banco::conectar();
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$sql = "SELECT * FROM receitas WHERE id = ?";
+	$q = $pdo->prepare($sql);
+	$q->execute(array($id));
+	$data = $q->fetch(PDO::FETCH_ASSOC);
+	$titulo = $data['titulo'];
+	$ingredientes = $data['ingredientes'];
+	$modopreparo = $data['modopreparo'];
+	Banco::desconectar();
+}
 
 $lin = "10";
 $col = "100";
@@ -67,58 +84,67 @@ $col = "100";
 <html lang="pt-br">
 <head>
 	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="bootstrap/bootstrap.min.css" />
 	<link rel="shortcut icon" href="img/favicon.png" />
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="bootstrap/jquery.min.js"></script>
+	<script src="bootstrap/bootstrap.min.js"></script>
 	<title>Atualizar Receita</title>
 	<style>
-		.favicon {
-			width: 30px;
-		}
+		.favicon {width: 30px;}
+		.oculto {display: none;}
 	</style>
 </head>
 <body>
 
 <div class="jumbotron">
-  <div class="container">
-    <h2><img src="img/favicon.png" alt="Cupcake" class="favicon"> Atualizar Receita</h2>
-  </div>
+	<div class="container">
+		<h2><img src="img/favicon.png" alt="Cupcake" class="favicon"> Atualizar Receita</h2>
+	</div>
 </div>
 
 <div class="container">
-  <form class="form-horizontal" action="atualizar.php?id=<?php echo $id; ?>" method="post">
-    <div class="form-group">
+	<form class="form-horizontal" action="atualizar.php?id=<?php echo $id; ?>" method="post" enctype="multipart/form-data">
+		<div class="form-group">
 <?php echo !empty($tituloErro)?'error':'';?>
-      <label class="control-label col-sm-2" for="nome">Título:</label>
-      <div class="col-sm-10">
-        <input type="text" maxlength="50" class="form-control" id="nome" placeholder="Insira o título..." name="titulo" required value="<?php echo !empty($titulo)?$titulo:'';?>" />
+			<label class="control-label col-sm-2" for="nome">Título:</label>
+			<div class="col-sm-10">
+				<input type="text" maxlength="50" class="form-control" id="nome" placeholder="Insira o título..." name="titulo" required value="<?php echo !empty($titulo)?$titulo:'';?>" />
 <?php if (!empty($tituloErro)): ?><?php echo $tituloErro; ?><?php endif; ?>
-      </div>
-    </div>
-    <div class="form-group">
+			</div>
+		</div>
+		<div class="form-group">
 <?php echo !empty($ingredientesErro)?'error':'';?>
-      <label class="control-label col-sm-2" for="sbnm">Ingredientes:</label>
-      <div class="col-sm-10">          
-        <textarea id="sbnm" name="igrd" class="form-control" rows="<?php echo $lin; ?>" cols="<?php echo $col; ?>"><?php echo !empty($ingredientes)?$ingredientes:'';?></textarea>
-<?php if (!empty($ingredientesErro)): ?> <?php echo $ingredientesErro;?> <?php endif; ?>
-      </div>
-    </div>
-    <div class="form-group">
+			<label class="control-label col-sm-2" for="sbnm">Ingredientes:</label>
+				<div class="col-sm-10">          
+        			<textarea id="sbnm" name="igrd" class="form-control" rows="<?php echo $lin; ?>" cols="<?php echo $col; ?>"><?php echo !empty($ingredientes)?$ingredientes:'';?></textarea>
+<?php if (!empty($ingredientesErro)): ?><?php echo $ingredientesErro;?><?php endif; ?>
+				</div>
+		</div>
+		<div class="form-group">
 <?php echo !empty($modopreparoErro)?'error':'';?>
-      <label class="control-label col-sm-2" for="apld">Modo de Preparo:</label>
-      <div class="col-sm-10">          
-        <textarea name="modopreparo" class="form-control" id="apld" rows="<?php echo $lin; ?>" cols="<?php echo $col; ?>"><?php echo !empty($modopreparo)?$modopreparo:'';?></textarea>
-<?php if (!empty($modopreparoErro)): ?> <?php echo $modopreparoErro;?> <?php endif; ?>
-
-<br/>
-
-		<a href="index.php" class="btn btn-primary">Voltar</a>
-		<button class="btn btn-success">Atualizar</button>
-      </div>
-    </div>
-  </form>
+			<label class="control-label col-sm-2" for="apld">Modo de Preparo:</label>
+			<div class="col-sm-10">          
+				<textarea name="modopreparo" class="form-control" id="apld" rows="<?php echo $lin; ?>" cols="<?php echo $col; ?>"><?php echo !empty($modopreparo)?$modopreparo:'';?></textarea>
+<?php if (!empty($modopreparoErro)): ?><?php echo $modopreparoErro;?><?php endif; ?>
+				<br/>
+				<a href="index.php" class="btn btn-primary">Voltar</a>
+				<button class="btn btn-success">Atualizar</button>
+<?php if (file_exists($data['imagem'])) { ?>
+				<a href="apgimg.php?id=<?php echo $data['id']; ?>&pth=<?php echo $data['imagem']; ?>" class="btn btn-dark" target="_blank">Apagar imagem</a>
+<?php }
+else {
+?>
+				<label for="foto">
+					<div class="btn btn-info">Arquivo</div>
+				</label>
+				<input name="imgm" id="foto" type="file" class="oculto" />
+<?php
+}
+?>
+			</div>
+		</div>
+	</form>
 </div>
 
 </body>
