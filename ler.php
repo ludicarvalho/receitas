4b9062ed83115906eb55770f3e12b4abd08f9196
Ludi Carvalho <40408321+ludicarvalho@users.noreply.github.com>
2018-12-16 14:49:11 -0200

    Update ler.php

--- ler.php
+++ ler.php
@@ -3,98 +3,118 @@ include "banco.php";
 $id = null;
 
 if (!empty($_GET['id'])) {
-	$id = $_GET['id'];
+  $id = $_GET['id'];
 }
 
 if (null==$id) {
-	header("Location: index.php");
+  header("Location: index.php");
 }
 
 else {
-	$pdo = Banco::conectar();
-	$sql = "SELECT * FROM receitas WHERE id = ?";
-	$q = $pdo->prepare($sql);
-	$q->execute(array($id));
-	$data = $q->fetch(PDO::FETCH_ASSOC);
+$pdo = Banco::conectar();
+$sql = "SELECT * FROM receitas WHERE id = ?";
+$q = $pdo->prepare($sql);
+$q->execute(array($id));
+$data = $q->fetch(PDO::FETCH_ASSOC);
 }
+
+if (empty($_GET['pag']))
+	$pagina = 1;
+else
+	$pagina = $_GET['pag'];
 
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
 	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=0.9">
 	<link rel="shortcut icon" href="img/favicon.png" />
-	<link rel="stylesheet" href="bootstrap/bootstrap.css" />
+	<link rel="stylesheet" href="bootstrap/bootstrap.min.css" />
 	<script src="bootstrap/jquery.js"></script>
 	<title>Receita - <?php echo ucwords($data['titulo']); ?></title>
 	<style>
-	.footer {
-		background-color: #f5f5f5;
+	.footer, h2 {
+		background-color: #F5F5F5;
+	}
+	@media screen and (max-width: 400px) {
+		div.ali {
+			text-align: right;
+		}
 	}
 	</style>
 </head>
 <body>
 
 <!-- Menu -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-	<span class="navbar-brand">Receita&nbsp;</span>
-	<ul class="navbar-nav mr-auto">
-		<li class="nav-item active">
-			<a class="nav-link" href="index.php">Início</a>
-		</li>
-		<li class="nav-item dropdown">
-			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				Menu
-			</a>
-			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-				<a class="dropdown-item" href="atualizar.php?id=<?php echo $data['id']; ?>">Atualizar Receita</a>
-				<a class="dropdown-item" href="apagar.php?id=<?php echo $data['id']; ?>&usr=<?php echo ucwords($data['titulo']); ?>">Apagar Receita</a>
-			</div>
-		</li>
-	</ul>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+	<div class="container">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#me" aria-controls="me" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="me">
+			<ul class="navbar-nav">
+				<li>
+					<a class="navbar-brand" href="index.php">Início</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="navbar-brand dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Menu
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="atualizar.php?id=<?php echo $data['id']; ?>">Atualizar Receita</a>
+						<a class="dropdown-item" href="apagar.php?id=<?php echo $data['id']; ?>&nr=<?php echo ucwords($data['titulo']); ?>">Apagar Receita</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
 </nav>
 <!-- -->
 
-<br/>
 <div class="jumbotron">
 	<div class="container">
-		<h1><?php echo ucwords($data['titulo']); ?></h1>
+		<h1 class="text-center"><?php echo ucwords($data['titulo']); ?></h1>
 	</div>
 </div>
 
 <div class="container">
     <ul class="list-group">
-		<li class="list-group-item bg-dark">
-			<h1 class="text-center text-white">Ingredientes</h1>
-		</li>
-		<li class="list-group-item">
+    <li class="list-group-item bg-dark">
+      <h1 class="text-center text-white">Ingredientes</h1>
+    </li>
+
+    <li class="list-group-item">
 <?php
 echo str_replace(";", "\n</li>\n\n<li class='list-group-item'>", $data['ingredientes']);
 ?>
 
-    	</li>
+    </li>
     </ul>
-
 <br/>
 
-	<ul class="list-group">
-		<li class="list-group-item bg-dark">
-			<h1 class="text-center text-white">Modo de Preparo</h1>
-		</li>
-		<li class="list-group-item">
+<ul class="list-group">
+    <li class="list-group-item bg-dark">
+      <h1 class="text-center text-white">Modo de Preparo</h1>
+    </li>
+    
+    <li class="list-group-item">
 <?php
 echo str_replace(";", "\n</li>\n<li class='list-group-item'>", $data['modopreparo']);
 ?>
 
-		</li>
-	</ul>
-	<a href="index.php" class="btn btn-primary">Voltar</a>
+    </li>
+    </ul>
+<br/>
+	<div class="ali">
+		<a href="index.php?pagina=<?php echo $pagina; ?>" class="btn btn-secondary">Voltar</a>
+	</div>
 </div>
 
 <br/>
 
 <?php rodape(); ?>
+
 
 <!-- Bootstrap core JavaScript
 ================================================== -->
@@ -103,6 +123,5 @@ echo str_replace(";", "\n</li>\n<li class='list-group-item'>", $data['modoprepar
 <script>window.jQuery || document.write('<script src="bootstrap/jquery-slim.min.js"><\/script>')</script>
 <script src="bootstrap/popper.min.js"></script>
 <script src="bootstrap/bootstrap.min.js"></script>
-
 </body>
 </html>
